f299ab4b751e717df23588acb0045a58a7680f59
brunoDEVJ <bruno.devj@gmail.com>
2022-10-21 17:04:10 -0300

    cc

--- testes.js
+++ testes.js
@@ -4,12 +4,41 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 8; // 512
 
-let px = 0
 
-function anima()
-{
-    requestAnimationFrame(anima)
 
-    c.fillRect(px,0,25,25)
-    c.fillStyle = 'black'
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let px = 0
+
+// const div = document.querySelector('.div')
+
+// setInterval(() => {
+//     px++
+//     div.style.marginLeft = `${px}px`
+//     console.log(px)
+// }, 300);
